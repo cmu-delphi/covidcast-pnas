0ed68e8e434f2057e8d68ee0c77b80f0d9bd88df
Daniel J. McDonald <dajmcdon@gmail.com>
2021-06-16 06:56:00 -0700

    Revise dated and add figs marked TODO to manuscript.

--- forecast/code/revisions-dv-jhu.R
+++ forecast/code/revisions-dv-jhu.R
@@ -19,7 +19,9 @@ dv_as_of <-  map_dfr(as_ofs, function(as_of) {
 
 
 pp <- bind_rows(cases_as_of, dv_as_of) %>% 
-  mutate(as_of = fct_relabel(factor(as_of), function(x) strftime(x, "%b %d"))) %>%
+  mutate(as_of = fct_relabel(factor(as_of), function(x) strftime(x, "%b %d")),
+         data_source = recode(data_source, `doctor-visits` = "DV-CLI",
+                              `jhu-csse` = "JHU-CSSE")) %>%
   ggplot(aes(x = time_value, y = value)) + 
   geom_line(aes(color = factor(as_of))) + 
   facet_wrap(~data_source, scales = "free_y") +
