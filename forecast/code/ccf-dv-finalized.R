20b35e977f5e826a30b690087726f6d2d8ed676b
Daniel J. McDonald <dajmcdon@gmail.com>
2021-06-16 09:52:53 -0700

    Fix some figure titles.

--- forecast/code/ccf-dv-finalized.R
+++ forecast/code/ccf-dv-finalized.R
@@ -15,16 +15,17 @@ names(sigs)[3:4] <- c("cases", "doctor-visits")
 
 std_sigs <- sigs %>%
   mutate(across(c("cases","doctor-visits"), ~ (.x - mean(.x)) / sd(.x))) %>%
+  rename(`JHU-CSSE` = cases, `DV-CLI` = `doctor-visits`) %>%
   select(-geo_value) %>%
   pivot_longer(-time_value)
   
 g1 <- std_sigs %>%
   ggplot(aes(time_value, value, color = name)) +
   geom_line() +
-  theme_bw(base_size = 14) +
-  xlab("date") + ylab("standardized signal") +
+  theme_bw() +
+  xlab("Date") + ylab("Standardized signal") +
   scale_color_viridis_d(begin=.25, end=.75, name = "") +
-  theme(legend.position = "right")
+  theme(legend.position = "bottom")
 
 cc <- ccf(sigs$`doctor-visits`, sigs$cases, plot = FALSE)
 g2 <- ggplot(tibble(lag = drop(cc$lag), ccf = drop(cc$acf)), aes(lag)) +
@@ -33,9 +34,13 @@ g2 <- ggplot(tibble(lag = drop(cc$lag), ccf = drop(cc$acf)), aes(lag)) +
              color = "darkorange", linetype = "dashed") +
   geom_hline(yintercept = 0) +
   geom_vline(xintercept = 0) +
-  ylab("cross correlation") +
-  theme_bw(base_size = 14) 
+  ylab("Cross correlation") +
+  xlab("Lag") +
+  theme_bw() +
+  theme(legend.position = "none")
 
+leg <- cowplot::get_legend(g1)
 
-gg <- cowplot::plot_grid(g2, g1, rel_widths = c(4,6))
+gg <- cowplot::plot_grid(g2, g1 + theme(legend.position = "none"))
+gg <- cowplot::plot_grid(gg, leg, nrow = 2, rel_heights = c(1,.1))
 
