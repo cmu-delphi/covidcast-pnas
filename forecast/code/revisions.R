e61179ac2038de1ed388c402cf33be777a1d9f74
Ryan Tibshirani <ryantibs@gmail.com>
2021-06-17 22:48:42 -0400

    Plot updates

--- forecast/code/revisions.R
+++ forecast/code/revisions.R
@@ -37,5 +37,5 @@ bind_rows(cases_as_of, dv_as_of) %>%
   theme(legend.position = "bottom") +
   scale_color_viridis_d(end = .9, begin = .1)
 
-ggsave("../paper/fig/revisions.pdf", width = 5.5, height = 4)
+ggsave("../paper/fig/revisions.pdf", width = 5, height = 4)
 
--- forecast/code/trajectory.R
+++ forecast/code/trajectory.R
@@ -1,26 +1,14 @@
 library(covidcast)
+library(evalcast)
 library(tidyverse)
 
-# ny_preds <- readRDS(file.path(path_to_data, "predictions_honest.RDS")) %>%
-#   filter(geo_value == "303", 
-#          forecast_date == "2020-10-15",
-#          forecaster == "AR3") %>%
-#   mutate(target_end_date = forecast_date + days(ahead),
-#          incidence_period = "day")
-# 
-# ny_actuals <- readRDS(file.path(path_to_data, "actuals.RDS")) %>%
-#   filter(geo_value == "303", 
-#          target_end_date > "2020-09-01", 
-#          target_end_date < "2020-11-15") %>%
-#   rename(value = actual, time_value = target_end_date)
-#   
 ny_preds <- readRDS("../data/ny_predictions.RDS")
 ny_actuals <- readRDS("../data/ny_actuals.RDS")
 
 pd <- evalcast:::setup_plot_trajectory(ny_preds, side_truth = ny_actuals)
 pd$truth_df <- pd$truth_df %>% rename(target_end_date = time_value)
 
-gg <- ggplot(pd$truth_df, mapping = aes(x = target_end_date)) +
+ggplot(pd$truth_df, mapping = aes(x = target_end_date)) +
   geom_ribbon(
     data = pd$quantiles_df,
     mapping = aes(ymin = lower, ymax = upper, fill = interval)) +
@@ -30,11 +18,14 @@ gg <- ggplot(pd$truth_df, mapping = aes(x = target_end_date)) +
   geom_line(data = pd$points_df, 
             mapping = aes(y = value),
             color = "orange", size = .5) +
-  geom_point(data = pd$points_df, 
+  geom_point(data = pd$points_df,
             mapping = aes(y = value),
             color = "orange", size = 1) +
   geom_vline(xintercept = as.Date("2020-10-15")) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
   theme_bw(base_size = 14) + 
   theme(legend.position = "none") + 
-  ylab("AR forecast for case rate in NYC") + 
-  xlab("date")
+  ylab("Cases per 100,000 people") + 
+  xlab("Date")
+
+ggsave("../paper/fig/trajectory.pdf", width = 6, height = 4)
