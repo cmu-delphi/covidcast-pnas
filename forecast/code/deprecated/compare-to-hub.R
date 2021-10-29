3c68eb87943c9178f02aa3b7392a0b7b1bcc88cb
Daniel J. McDonald <dajmcdon@gmail.com>
2021-10-28 18:38:40 -0700

    updated hub comparison with all data

--- forecast/code/deprecated/compare-to-hub.R
+++ forecast/code/deprecated/compare-to-hub.R
@@ -40,12 +40,22 @@ case_scores <- case_scores %>% filter(forecaster %in% keepers)
 
 # Load our models ---------------------------------------------------------
 
-ours <- readRDS("~/Downloads/results_no_october_honest.RDS")
+ours <- readRDS("~/Downloads/results_honest.RDS")
 pop <- covidcast::state_census %>% select(ABBR, POPESTIMATE2019) %>%
   mutate(geo_value = tolower(ABBR)) %>% select(-ABBR)
 # scale from prop to num
-ours <- left_join(ours, pop) %>% mutate(wis = wis * POPESTIMATE2019 / 1e5 * 7) %>%
-  select(-ae, -POPESTIMATE2019)
+ours <- left_join(ours, pop) %>%
+  mutate(wis = wis * POPESTIMATE2019 / 1e5 * 7,
+         forecaster = recode(forecaster,
+                             AR3 = "AR",
+                             AR3CHCLI3 = "CHNG-CLI",
+                             AR3CHCOV3 = "CHNG-COVID",
+                             AR3DVCLI3 = "DV-CLI",
+                             AR3FBCLI3 = "CTIS-CLIIC",
+                             AR3GSSAA3_Subset = "drop",
+                             AR3GSSAA3_Zero = "Google-AA")) %>%
+  filter(forecaster != "drop") %>%
+  select(-ae, -POPESTIMATE2019) 
 
 common_fd <- as.Date(intersect(
   case_scores %>% select(forecast_date) %>% distinct() %>% pull(),
@@ -57,71 +67,36 @@ our_models <- ours %>% filter(forecaster != "Baseline")
 baseline <- ours %>% filter(forecaster == "Baseline") %>%
   select(-forecaster) %>% rename(strawman_wis = wis)
 our_models <- left_join(our_models, baseline)
-all_models <- case_scores %>% bind_rows(our_models) 
+all_models <- bind_rows(hub = case_scores, ours = our_models, .id = "source") 
 
 
 all_time_performance <- all_models %>%
   filter(forecast_date %in% common_fd) %>%
-  group_by(forecaster, ahead) %>%
+  group_by(forecaster, ahead, source) %>%
   summarise(rel_wis = Mean(wis) / Mean(strawman_wis),
-            geo_wis1 = GeoMean((wis + 1) / (strawman_wis + 1)),
-            geo_wis = GeoMean(wis / strawman_wis))
+            geo_wis = GeoMean((wis + 1) / (strawman_wis + 1))) %>%
+  pivot_longer(contains("wis"))
 
-all_time_performance %>%
-  pivot_longer(contains("wis")) %>%
-  ggplot(aes(ahead, value, group = forecaster)) +
+facet_labs <- c(geo_wis = "Geometric mean of WIS", rel_wis = "Mean of WIS")
+fcast_colors <- c(RColorBrewer::brewer.pal(5, "Set1"), "#000000")
+names(fcast_colors) <- c("CHNG-CLI", "CHNG-COVID", "CTIS-CLIIC", "DV-CLI",
+                         "Google-AA", "AR")
+
+
+ggplot(all_time_performance %>% filter(source == "ours"),
+       aes(ahead, value, color = forecaster)) +
+  geom_line(data = all_time_performance %>% filter(forecaster != "COVIDhub-ensemble"), 
+            aes(group = forecaster),
+            color = "grey80") +
+  geom_line(data = all_time_performance %>% filter(forecaster == "COVIDhub-ensemble"),
+            color = "lightblue", size = 1.5) +
+  geom_line(aes(ahead, value, color = forecaster)) + 
+  geom_point(aes(ahead, value, color = forecaster)) +
+  scale_color_manual(values = fcast_colors, guide = guide_legend(nrow = 1)) +
   theme_bw() +
-  geom_line(color = "grey20") +
-  geom_point(color = "grey20") +
-  scale_color_viridis_d() +
-  facet_wrap(~name) +
-  geom_hline(yintercept = 1, color = "red") +
-  theme(legend.position = "bottom", legend.title = element_blank()) +
-  scale_y_log10()
-
-df <- summarizer(fcasts_honest %>% filter(period != "jm") %>%
-                   group_by(ahead, forecaster), "wis", NULL, 
-                 "strawman_wis", Mean, c("aggr","scale"))
-
-GeoMean1 <- function(x) exp(mean(log(x + 1), na.rm = TRUE))
-
-all_time_performance <- all_time_performance %>%
-  filter(forecaster != "COVIDhub-4_week_ensemble")
-
-ggplot(df) +
-  geom_line(aes(ahead, wis, color = forecaster)) + 
-  geom_point(aes(ahead, wis, color = forecaster)) +
-  theme_bw() +
+  ylab("relative to baseline") +
   geom_hline(yintercept = 1, size = 1.5) +
   xlab("Days ahead") +
-  ylab("Mean WIS (relative to baseline)") +
-  scale_color_manual(values = fcast_colors, guide = guide_legend(nrow = 1)) +
-  theme(legend.position = "bottom", legend.title = element_blank()) +
-  geom_line(data = all_time_performance %>% filter(ahead < 4, forecaster != "COVIDhub-ensemble"), 
-            aes(ahead * 7, rel_wis, group = forecaster),
-            color = "grey70") +
-  geom_line(data = all_time_performance %>% filter(ahead < 4, forecaster == "COVIDhub-ensemble"),
-            aes(ahead * 7, rel_wis), color = "lightblue", size = 1.5) +
-  scale_y_log10()
+  facet_wrap(~ name, labeller = labeller(name = facet_labs)) +
+  theme(legend.position = "bottom", legend.title = element_blank())
 
-df2 <- fcasts_honest %>% 
-  filter(period != "jm") %>%
-  group_by(ahead, forecaster) %>%
-  summarise(wis = GeoMean((wis + 1) / (strawman_wis + 1)))
-
-
-ggplot(df2) +
-  geom_line(aes(ahead, wis, color = forecaster)) + 
-  geom_point(aes(ahead, wis, color = forecaster)) +
-  theme_bw() +
-  geom_hline(yintercept = 1, size = 1.5) +
-  xlab("Days ahead") +
-  ylab("Geometric mean of WIS (relative to baseline)") +
-  scale_color_manual(values = fcast_colors, guide = guide_legend(nrow = 1)) +
-  theme(legend.position = "bottom", legend.title = element_blank()) +
-  geom_line(data = all_time_performance %>% filter(ahead < 4, forecaster != "COVIDhub-ensemble"), 
-            aes(ahead * 7, geo_wis1, group = forecaster),
-            color = "grey70") +
-  geom_line(data = all_time_performance %>% filter(ahead < 4, forecaster == "COVIDhub-ensemble"),
-            aes(ahead * 7, geo_wis1), color = "lightblue", size = 1.5) +
-  scale_y_log10()
